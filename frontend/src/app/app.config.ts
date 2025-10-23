aafac9b108a289077f9f0202609c4214f307b7e7
Marlon <marlonfda@gmail.com>
2025-10-22 22:37:21 -0300

    feat(api-integration): Classes e configurações para integração com a api

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
+import { PoHttpRequestModule } from '@po-ui/ng-components';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom([PoHttpRequestModule]),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptors([errorInterceptor]))
+  ],
+  
 };
